d9a3fd02c94126a20219b97aca5a893ebdc708a7
RafehSaeed <rafeh_saeed@hotmail.ca>
2016-04-10 12:32:58 -0400

    Authentication Completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,55 +1,88 @@
 
-# Creating Login for the Users
+	# Creating Login for the Users
 
-class SessionsController < ApplicationController
+	class SessionsController < ApplicationController
 
 
 
-  def new
+	  def new
 
-  end
 
-  def create
+	  end
 
-  	 user = Buisness.find_by(email: params[:session][:email]) unless (params[:session] == nil)
-   	if user
-   		#log the user in
-   		log_in(user);
-   		render json: user
+	  #Creates a session for the user being logged in
+	  def create
 
-   		
-   	else
-   		
-   		render json: {"content":"Doesnt Exist"}
-  end
-end
+	  	#User can either login as a Buisness Admin or Service Provider
+	  	 user = Buisness.find_by(email: params[:session][:email]) unless (params[:session] == nil )
+	  	 user = Admin.find_by(email: params[:session][:email]) unless (params[:session] == nil || user!=nil)
+	  	 user = ServiceProvider.find_by(email: params[:session][:email]) unless (params[:session] == nil || user!=nil)
+	  	 
+	   	if user && !logged_in?
+	   		#log the user in
+	   		log_in(user)
+	   		remember(user)
+	   		current_user
+	   		
+	   		print logged_in?;
+	   		render json: current_user
 
-  def destroy
-  	log_out if logged_in?
- 	render json: "User Logged out"
-  end
 
-  private
 
-  def log_in(user)
-		session[:user_id]= user.id
+	   		
+	   	elsif user == nil
+	   			render json: {"content":"Doesnt Exist"}
+	   	else
+	   		render json: {"content":"User already logged in"}
+	   			
+	  end
 	end
 
+
+	  #used for logging a user out via a delete http request
+	  def destroy
+	  	log_out if logged_in?
+	 	render json: "User Logged out"
+	  end
+
+	  private
+
+	  #logs the user in and creates a session for him
+
+	  def log_in(user)
+	  		current_user
+			session[:user_id]= user.id
+
+		end
+
+	  #Returns the current user in the session
 	  def current_user
-    @current_user ||= Buisness.find_by(id: session[:user_id])
- 	 end
+	     @current_user ||= Buisness.find_by(id: session[:user_id])
 
- 	   # Returns true if the user is logged in, false otherwise.
-  def logged_in?
-    !current_user.nil?
-  end
+	    
+	 	 end
 
-  def log_out
-  	
-  	cookies.delete(:user_id)
-    session.delete(:user_id)
-    @current_user = nil
+	  #Returns true if the user is logged in, false otherwise.
+	  def logged_in?
+	     !current_user.nil?
 
-  end
-end
+	  end
+
+	  #Logs the user out
+	  def log_out
+	  	
+	  	cookies.delete(:user_id)
+	    session.delete(:user_id)
+	    @current_user = nil
+
+	  end
+
+	 # Remembers a user in a persistent session.
+	  def remember(user)
+
+	    cookies.permanent.signed[:user_id] = user.id
+
+	  end
+
+	end
 
